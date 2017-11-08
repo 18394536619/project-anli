b11f83e86bcf1086158a2af34b2117874b0a334a
plusyu <809501557@qq.com>
2017-11-08 10:43:24 +0800

    this is now project

---
+++ waimao-demo/js/index.js
@@ -0,0 +1,26 @@
+
+var submits=document.getElementsByClassName("submit");
+var inputs=document.getElementsByTagName("input");
+// var form=document.getElementsByTagName('form')[0];
+var form=document.getElementsByTagName("form")[0];
+var pt=/^\d{11}$/;
+var pwds=/^[a-zA-Z]\w{5,17}$/;
+form.onsubmit=function(event){
+    var inputs=document.getElementsByTagName("input");
+    inputs=Array.prototype.slice.call(inputs,0);
+        if(pt.test(inputs[0].value)){
+            if(pwds.test(inputs[1].value)){
+                alert("登陆成功");
+                 event.preventDefault();
+                window.location.href="file:///home/briup/WebUI/%E9%A1%B9%E7%9B%AE/waimao-demo/homePage.html";
+               
+            }else{
+                inputs[1].parentNode.style.borderColor="red";
+                alert("密码有误，请重输");
+                event.preventDefault();
+            }
+        }else{
+            inputs[0].parentNode.style.borderColor="red";
+             event.preventDefault();
+        }
+}
